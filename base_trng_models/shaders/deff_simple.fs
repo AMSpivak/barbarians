3c5b14f6da0caa1e2168fc9139fcc2352f2e55be
Spivak Andrey <amspivak@mail.ru>
2018-03-06 17:25:57 +0300

    defeered simple

--- base_trng_models/shaders/deff_simple.fs
+++ base_trng_models/shaders/deff_simple.fs
@@ -7,17 +7,29 @@ uniform sampler2D DiffuseMap;
 uniform sampler2D NormalMap;
 uniform sampler2D PositionMap;
 
-//uniform mat4 lightSpaceMatrix;
-
 uniform vec4 LightLocation;
 uniform vec3 LightColor;
 uniform vec3 viewPos;
+
+float GGX_PartialGeometry(float cosThetaN, float alpha) {
+    float cosTheta_sqr = clamp(cosThetaN*cosThetaN, 0.0, 1.0);
+    float tan2 = ( 1 - cosTheta_sqr ) / cosTheta_sqr;
+    float GP = 2 / ( 1 + sqrt( 1 + alpha * alpha * tan2 ) );
+    return GP;
+}
+
+float GGX_Distribution(float cosThetaNH, float alpha) {
+    float alpha2 = alpha * alpha;
+    float NH_sqr = clamp(cosThetaNH*cosThetaNH, 0.0, 1.0);
+    float den = NH_sqr * alpha2 + (1.0 - NH_sqr);
+    return alpha2 / ( M_PI * den * den );
+}
 
 void main()
 {
     float rad = LightLocation.w;
 	vec4 texColor = texture(DiffuseMap, TexCoords);
-    if(texColor.a < 0.1)
+    if(texColor.a < 0.05)
         discard;
 
     vec3 FragPos= texture(PositionMap, TexCoords).xyz;
@@ -28,22 +40,28 @@ void main()
     vec3 LightVec = LightLocation.xyz - FragPos;
     vec3 LightDir = normalize(LightVec);
 	vec3 texNormal= normal_map.xyz;
-    vec3 reflectDir= reflect(-LightDir, texNormal).xyz;
+
     float norm_l = max(dot(texNormal,LightDir),0);
 
-    vec3 halfwayDir = normalize(LightDir + viewDir);  
-    float spec = 10.0 * pow(max(dot(texNormal, halfwayDir), 0.0), 16.0);
-    //float spec = pow(max(dot(viewDir, reflectDir), 0.0), 10);
-    
-    //norm_l = smoothstep(0.45,0.55,norm_l);
-    //norm_l *=  max(LightLocation.w - length(LightVec),0);//(smoothstep(LightLocation.w*0.3,LightLocation.w,length(LightDir)));
-    norm_l *=  1.0 - smoothstep(rad*0.3,rad,length(LightVec));
-    norm_l += normal_map.w * spec;
-    
-    //norm_l = smoothstep(0.45,0.55,norm_l);
-    float res = norm_l;
+    vec3 halfwayDir = normalize(LightDir + viewDir); 
+    float dotHV = max(dot(viewDir, halfwayDir), 0.0);
+    float dotNH = max(dot(texNormal, halfwayDir), 0.0);
+    float dotNV = max(dot(texNormal, viewDir), 0.0);
 
-    //float res    = smoothstep(0.45,0.55,norm_l);
-    //FragColor =vec4( texColor.xyz,1.0);
-    FragColor =vec4((res) * LightColor * vec3(1.0,1.0,1.0),1.0);//texColor;// LightDir.y*(0.3 +0.7*(shadow_res) *norm_l) * texColor;
+    float f0 = mix(0.004,0.7,texColor.a);
+    float shlick =f0 + (1.0-f0)*pow((1.0 -norm_l),5);
+    float roug_sqr = normal_map.w*normal_map.w;
+    float G = GGX_PartialGeometry(dotNV, roug_sqr) * GGX_PartialGeometry(norm_l, roug_sqr);
+    float D = GGX_Distribution(dotNH, roug_sqr);    
+
+    float spec = G*shlick*D*0.25/(max(0.0, dotNV)+0.001);
+    spec =max(0.0, spec);
+
+
+    float intensivity = 1.0 - smoothstep(rad*0.3,rad,length(LightVec));
+    float diffuse = clamp(1.0 - shlick, 0.0, 1.0);
+
+    float res = intensivity *(diffuse*norm_l/M_PI + spec);
+
+    FragColor =vec4((res) * LightColor * vec3(1.0,1.0,1.0),1.0);
 }
